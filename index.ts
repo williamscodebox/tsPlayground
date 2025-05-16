0032b872df048809f2c87ad6dcae9c53ac028bc1
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-16 00:43:40 -0400

    edited index

--- index.ts
+++ index.ts
@@ -11,3 +11,23 @@ console.log(person.name);
 let age: number = 30;
 console.log(age);
 console.log(typeof age);
+
+class Character {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(
+      `Hello, my name is ${this.name} and I am ${this.age} years old.`
+    );
+  }
+}
+let p1 = new Character("Alice", 25);
+
+console.log(p1.name);
+p1.greet();
