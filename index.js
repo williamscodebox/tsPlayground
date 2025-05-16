2c312a085fc97068610a0352e6ea3d92a83ce427
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-16 11:54:46 -0400

    added tsconfig

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+const person = {
+  name: "John",
+};
+console.log(person.name);
+let age = 30;
+console.log(age);
+console.log(typeof age);
+class Character {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  greet() {
+    console.log(
+      `Hello, my name is ${this.name} and I am ${this.age} years old.`
+    );
+  }
+}
+let p1 = new Character("Alice", 25);
+console.log(p1.name);
+p1.greet();
